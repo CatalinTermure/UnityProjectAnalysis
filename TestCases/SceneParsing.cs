afeb51b2e2a9f1db62adc6a183fd964d844d763e
CatalinTermure <cataliin2002@gmail.com>
2023-12-25 20:18:10 +0200

    Implement fetching scene paths.

---
+++ TestCases/SceneParsing.cs
@@ -0,0 +1,16 @@
+﻿using UnityProjectAnalysis;
+
+namespace TestCases;
+
+public class SceneParsing
+{
+    [Test]
+    public void ScenePathsFound()
+    {
+        var projectAnalyzer = new ProjectAnalyzer("tests/TestCase01");
+        List<string> scenes = projectAnalyzer.GetScenePaths();
+        Assert.That(scenes, Has.Count.EqualTo(2));
+        Assert.That(scenes, Has.One.SamePath("Assets/Scenes/SampleScene.unity"));
+        Assert.That(scenes, Has.One.SamePath("Assets/Scenes/SecondScene.unity"));
+    }
+}
---
+++ UnityProjectAnalysis/ProjectAnalyzer.cs
@@ -0,0 +1,19 @@
+﻿namespace UnityProjectAnalysis;
+
+public class ProjectAnalyzer
+{
+    private readonly string _projectPath;
+
+    public ProjectAnalyzer(string projectPath)
+    {
+        _projectPath = projectPath;
+    }
+
+    public List<string> GetScenePaths()
+    {
+        string scenesDirectory = Path.Combine(_projectPath, "Assets", "Scenes");
+        string[] scenePaths = Directory.GetFiles(scenesDirectory, "*.unity", SearchOption.AllDirectories);
+
+        return scenePaths.Select(scenePath => Path.GetRelativePath(_projectPath, scenePath)).ToList();
+    }
+}
